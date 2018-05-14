706bdf9c886952f8f6ad86928bf7033666a70283
Mi Hwangbo <mhwangbo@e1z3r3p21.42.us.org>
2018-05-13 20:33:14 -0700

    progress

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,15 +6,22 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 17:27:32 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/05/12 15:11:19 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/05/13 17:50:14 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-#include "../lib/Libft/libft.h"
-#include <math.h>
-#include <mlx.h>
+# include "../lib/Libft/libft.h"
+# include <math.h>
+# include <mlx.h>
+
+typedef struct		s_pos
+{
+	int		x;
+	int		y;
+	int		z;
+}					t_pos;
 
 typedef struct		s_env
 {
@@ -22,10 +29,11 @@ typedef struct		s_env
 	int		win_y;
 	int		x_max;
 	int		y_max;
-	int		x;
-	int		y;
+	int		gap;
+	int		z_scale;
 	void	*mlx_ptr;
 	void	*win_ptr;
+	t_pos	*pos;
 }					t_env;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/10 13:07:34 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/05/12 18:14:29 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/05/13 20:13:17 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ int		*fdf_split(char *line, t_env *e)
 	char	**tmp;
 	int		x;
 
-	printf("LINE: %s\n", line);
 	line_i = ft_memalloc(sizeof(int) * e->x_max + 1);
 	tmp = ft_strsplit(line, ' ');
 	x = -1;
@@ -61,14 +60,122 @@ int		**fdf_read(char *av, t_env *e)
 	fd = open(av, O_RDONLY);
 	y = -1;
 	file = (int**)malloc(sizeof(int*) * (e->y_max + 1));
-	while (get_next_line(fd, &line) == 1 && ++y < e->y_max)
+	while (get_next_line(fd, &line) > 0 && ++y < e->y_max)
 	{
 		file[y] = fdf_split(line, e);
 		free(line);
 	}
+	if (get_next_line(fd, &line) == 0 && ++y < e->y_max)
+		file[y] = fdf_split(line, e);
 	file[y] = NULL;
 	close(fd);
 	return (file);
+}
+
+void	fdf_put_struct(int **line, t_env *e)
+{
+	int		y;
+	int		x;
+	int		i;
+
+	e->pos = ft_memalloc(sizeof(t_pos) * (e->y_max * e->x_max) + 1);
+	ft_bzero(e->pos, sizeof(t_pos) * (e->y_max * e->x_max) + 1);
+	y = -1;
+	i = 0;
+	while (++y < e->y_max)
+	{
+		x = -1;
+		while (++x < e->x_max)
+		{
+			e->pos[i].x = x;
+			e->pos[i].y = y;
+			e->pos[i++].z = line[y][x];
+		}
+	}
+}
+
+/*
+void	fdf_draw(t_env *e)
+{
+	int		i;
+	int		tmp_gap;
+
+	i = -1;
+	if (e->x_max >= e->y_max)
+	{
+		e->gap = e->win_x/e->x_max;
+		tmp_gap = e->win_y - (e->gap * (e->y_max - 1));
+		while (++i < (e->y_max * e->x_max))
+		{
+			mlx_pixel_put(e->mlx_ptr, e->win_ptr, ((e->pos[i].x * e->gap) + e->gap/2) + (e->pos[i].z * e->z_scale), ((e->pos[i].y * e->gap) + tmp_gap/2) - (e->pos[i].z * e->z_scale), 0xFFFFFF);
+		}
+	}
+	else if (e->y_max > e->x_max)
+	{
+		e->gap = e->win_y/e->y_max;
+		tmp_gap = e->win_x - (e->gap * (e->x_max - 1));
+		while (++i < (e->y_max * e->x_max))
+		{
+			mlx_pixel_put(e->mlx_ptr, e->win_ptr, ((e->pos[i].x * e->gap) + tmp_gap/2) + (e->pos[i].z * e->z_scale), ((e->pos[i].y * e->gap) + e->gap/2) - (e->pos[i].z * e->z_scale), 0xFFFFFF);
+		}
+	}
+}
+*/
+
+void	fdf_draw_y(t_env *e, int i, int tmp_gap)
+{
+	int		x[3];
+	int		y[3];
+
+	if ((i + e->x_max) < (e->x_max * e->y_max))
+	{
+		x[0] = ((e->pos[i].x * e->gap) + e->gap/2) + (e->pos[i].z * e->z_scale);
+		x[1] = x[0];
+		x[2] = ((e->pos[i + (e->x_max)].x * e->gap) + e->gap/2) + (e->pos[i + (e->x_max)].z * e->z_scale);
+		y[0] = ((e->pos[i].y * e->gap) + tmp_gap/2) - (e->pos[i].z * e->z_scale);
+		y[2] = ((e->pos[i + (e->x_max)].y * e->gap) + tmp_gap/2) - (e->pos[i + (e->x_max)].z * e->z_scale);
+		y[1] = y[0];
+		while (y[0] <= y[2])
+		{
+			mlx_pixel_put(e->mlx_ptr, e->win_ptr, ((x[2] - x[1])/(y[2] - y[1])) * (y[0] - y[1]) + x[1], y[0], 0xFFFFFF);
+			y[0]++;
+		}
+	}
+}
+
+void	fdf_draw(t_env *e)
+{
+	int		x[3];
+	int		y[3];
+	int		i;
+	int		tmp_gap;
+
+	i = -1;
+	e->gap = e->win_x/e->x_max;
+	tmp_gap = e->win_y - (e->gap * (e->y_max - 1));
+	while (++i < (e->y_max * e->x_max))
+	{
+		if (i + 1 < (e->y_max * e->x_max))
+			(e->pos[i].y != e->pos[i + 1].y) ? i++ : 0;
+		x[0] = ((e->pos[i].x * e->gap) + e->gap/2) + (e->pos[i].z * e->z_scale);
+		x[1] = x[0];
+		x[2] = ((e->pos[i + 1].x * e->gap) + e->gap/2) + (e->pos[i + 1].z * e->z_scale);
+		y[0] = ((e->pos[i].y * e->gap) + tmp_gap/2) - (e->pos[i].z * e->z_scale);
+		y[2] = ((e->pos[i + 1].y * e->gap) + tmp_gap/2) - (e->pos[i + 1].z * e->z_scale);
+		y[1] = y[0];
+		while (x[0] <= x[2])
+		{
+			mlx_pixel_put(e->mlx_ptr, e->win_ptr, x[0], ((y[2] - y[1])/(x[2] - x[1])) * (x[0] - x[1]) + y[1], 0xFFFFFF);
+			x[0]++;
+		}
+		while (y[0] <= y[2])
+		{
+			if (y[2] - y[1] != 0)
+				mlx_pixel_put(e->mlx_ptr, e->win_ptr, ((x[2] - x[1])/(y[2] - y[1])) * (y[0] - y[1]) + x[1], y[0], 0xFFFFFF);
+			y[0]++;
+		}
+//		fdf_draw_y(e, i, tmp_gap);
+	}
 }
 
 int		main(int ac, char **av)
@@ -89,11 +196,14 @@ int		main(int ac, char **av)
 		return (0);
 	}
 	line = fdf_read(av[1], &e);
+	fdf_put_struct(line, &e);
 	e.mlx_ptr = mlx_init();
 	e.win_x = ft_atoi(av[2]);
 	e.win_y = ft_atoi(av[3]);
+	e.z_scale = 2;
 	e.win_ptr = mlx_new_window(e.mlx_ptr, e.win_x, e.win_y, "FdF");
-	mlx_pixel_put(e.mlx_ptr, e.win_ptr, e.win_x/2, e.win_y/2, 0xFFFFFF);
+	fdf_draw(&e);
 	mlx_loop(e.mlx_ptr);
+	free(line);
 	return (0);
 }
